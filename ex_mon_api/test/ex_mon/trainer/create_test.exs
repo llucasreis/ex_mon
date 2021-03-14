8850948978d688d33ecb8220a650f01089d94199
Lucas Pereira Reis <lucasreis_15_@hotmail.com>
2021-03-14 17:36:58 -0400

    feat: create tests

---
+++ ex_mon_api/test/ex_mon/trainer/create_test.exs
@@ -0,0 +1,31 @@
+defmodule ExMon.Trainer.CreateTest do
+  use ExMon.DataCase
+
+  alias ExMon.{Repo, Trainer}
+  alias Trainer.Create
+
+  describe "call/1" do
+    test "when all params are valid, creates a trainer" do
+      params = %{name: "Lucas", password: "123456"}
+
+      count_before = Repo.aggregate(Trainer, :count)
+
+      response = Create.call(params)
+
+      count_after = Repo.aggregate(Trainer, :count)
+
+      assert {:ok, %Trainer{name: "Lucas"}} = response
+
+      assert count_after > count_before
+    end
+
+    test "when there are invalid params params, returns an error" do
+      params = %{name: "Lucas"}
+
+      response = Create.call(params)
+
+      assert {:error, changeset} = response
+      assert errors_on(changeset) == %{password: ["can't be blank"]}
+    end
+  end
+end
